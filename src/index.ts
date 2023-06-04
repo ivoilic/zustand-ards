465269095a80b8db5d7aa518b5d686a425b5c8ee
Ivo Ilić <ilicivo@gmail.com>
2023-06-03 20:29:50 -0400

    Updated createShallowHook to return a full version of the original hook

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
+import type { StoreApi, UseBoundStore } from 'zustand';
 import { shallow } from 'zustand/shallow';
 
-import { StoreApi, UseBoundStore } from 'zustand';
-
 /**
- * Creates a hook that allows for shallow access to the provided store.
+ * Creates a hook that allows for shallow access to the provided store by default.
  * This is effectively the same as passing `shallow` to the original store hook every time.
  */
 export const createShallowHook = <T>(useDeepStore: UseBoundStore<StoreApi<T>>) => {
-  return <U>(selector: (state: T) => U, equals?: (a: U, b: U) => boolean) =>
-    useDeepStore(selector, equals ?? shallow);
+  // Function Overloads
+  function useStore(): T;
+  function useStore<U>(selector: (state: T) => U, equals?: (a: U, b: U) => boolean): U;
+
+  // Actual function
+  function useStore<U>(selector?: (state: T) => U, equals?: (a: U, b: U) => boolean) {
+    return selector ? useDeepStore(selector, equals ?? shallow) : useDeepStore();
+  }
+  useStore.getState = useDeepStore.getState;
+  useStore.setState = useDeepStore.setState;
+  useStore.subscribe = useDeepStore.subscribe;
+  useStore.destroy = useDeepStore.destroy;
+
+  return useStore satisfies UseBoundStore<StoreApi<T>>;
 };
